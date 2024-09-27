f7f317af8a352771e20d748217a8acae065a8915
ShaxoCreative <140444349+ShaxoCreative@users.noreply.github.com>
2024-09-27 19:59:57 +0300

    Add files via upload

---
+++ dz_ku1/main.py
@@ -0,0 +1,30 @@
+from zipfile import ZipFile
+
+dirname = ""
+with ZipFile('files.zip', 'a') as myzip:
+    while True:
+        command = input('~' + dirname + '# ')
+        if command == 'ls':
+            for name in myzip.namelist():
+                print(name)
+        elif command == "exit":
+            exit(0)
+        elif command == "uname":
+            print("Linux")
+        elif command == "pwd":
+            print("/home/user")
+        elif command.startswith('cd'):
+            if len(command) == 2:
+                dirname = ""
+            else:
+                ndir = command.split(" ")[1]
+                if ndir in myzip.namelist():
+                    dirname = ndir
+                else:
+                    print("sh: cd: can't cd to " + ndir + ": No such file or directory")
+        elif command.startswith('touch '):
+            new_file = command.split()[1]
+            with myzip.open(new_file, 'w'):
+                pass
+        else:
+            print("Command not found.")
