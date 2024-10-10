42a29894eb6f7c2952c1e8f20076480a74a8d099
ShaxoCreative <140444349+ShaxoCreative@users.noreply.github.com>
2024-10-10 21:22:37 +0300

    Add files via upload

--- dz_ku1/main.py
+++ dz_ku1/main.py
@@ -1,33 +1,60 @@
+import tkinter as tk
+from tkinter import scrolledtext
 from zipfile import ZipFile
 
+root = tk.Tk()
+root.title("Shell Emulator")
+
 dirname = ""
-with ZipFile('files.zip', 'a') as myzip:
-    while True:
-        command = input('~' + dirname + '# ')
-        if command == 'ls':
-            for name in myzip.namelist():
-                print(name)
-        elif command == "exit":
-            exit(0)
-        elif command == "uname":
-            print("Linux")
-        elif command == "pwd":
-            if dirname == "":
-                print("/root")
-            else:
-                print("/root/" + dirname)
-        elif command.startswith('cd'):
-            if len(command) == 2:
-                dirname = ""
-            else:
-                ndir = command.split(" ")[1]
-                if ndir in myzip.namelist():
-                    dirname = ndir
-                else:
-                    print("sh: cd: can't cd to " + ndir + ": No such file or directory")
-        elif command.startswith('touch '):
-            new_file = command.split()[1]
-            with myzip.open(new_file, 'w'):
-                pass
+myzip = ZipFile('files.zip', 'a')
+
+
+def process_command():
+    global dirname
+    command = entry.get()
+    output = ""
+
+    if command == 'ls':
+        for name in myzip.namelist():
+            output += name + "\n"
+    elif command == "exit":
+        root.quit()
+    elif command == "uname":
+        output += "Linux\n"
+    elif command == "pwd":
+        if dirname == "":
+            output += "/root\n"
         else:
-            print("Command not found.")
+            output += "/root/" + dirname + "\n"
+    elif command.startswith('cd'):
+        if len(command) == 2:
+            dirname = ""
+        else:
+            ndir = command.split(" ")[1]
+            if ndir in myzip.namelist():
+                dirname = ndir
+            else:
+                output += "sh: cd: can't cd to " + ndir + ": No such file or directory\n"
+    elif command.startswith('touch '):
+        new_file = command.split()[1]
+        with myzip.open(new_file, 'w'):
+            pass
+    else:
+        output += "Command not found.\n"
+
+    output_text.insert(tk.END, "~" + dirname + "# " + command + "\n" + output)
+    entry.delete(0, tk.END)
+
+
+entry = tk.Entry(root, width=50)
+entry.grid(row=0, column=0, padx=10, pady=10)
+
+send_button = tk.Button(root, text="Enter", command=process_command)
+send_button.grid(row=0, column=1, padx=10, pady=10)
+
+output_text = scrolledtext.ScrolledText(root, width=70, height=20)
+output_text.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
+
+root.mainloop()
+
+myzip.close()
